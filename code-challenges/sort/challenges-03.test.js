7105d38e4140f8aeb4e81c24bb047fa6870558e1
Dayne <57049229+Gr8-Dayne@users.noreply.github.com>
2019-12-12 08:07:20 -0800

    Update challenges-03.test.js

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -149,9 +149,26 @@ If two people have the same full name, the younger one should come first. Do not
 ------------------------------------------------------------------------------------------------ */
 
 const sortPeopleBetter = (arr) => {
-  arr.sort((a,b) => {
-    
+  arr.sort(function(a,b){
+    if(a.lastName === b.lastName && a.firstName === b.firstName){
+      if(a.age > a.age){
+        return 1;
+      }else{
+        return -1;
+      }
+    }else if(a.lastName === b.lastName){
+      if(a.firstName > b.firstName){
+        return 1;
+      }else{
+        return -1;
+      }
+    }else if(a.lastName > b.lastName){
+      return 1;
+    }else{
+      return -1;
+    }
   })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
